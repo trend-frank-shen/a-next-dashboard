5f158be19151f6ec403850cb02afde5a2cfcc7f4
frank_shen <frank_shen@trendmicro.com>
2023-11-03 13:40:41 +0800

    feat: mutating data & handling error

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -168,6 +168,8 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log('invoice', invoice)
+
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
